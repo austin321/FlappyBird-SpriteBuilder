c80bf67536a8542788f4056e355e13bc98f46f74
austin321 <austinevanchen@gmail.com>
2014-12-29 10:15:16 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,13 +7,28 @@
 - (void)initialize
 {
     // your code here
-}
-
+}character = (Character*)[CCBReader load:@"Character"];
+[physicsNode addChild:character];
 -(void)update:(CCTime)delta
 {
     // put update code here
 }
 
 // put new methods here
-
-@end
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+}-(type I return)nameOfMethod: (type of first parameter) firstParameter continuationOfMethodName: (type of second parameter) secondParameter}@end
+-(void) doSomething
+{
+    NSLog(@"Hello World!");
+}
+-(int) doSomethingWithAString: (NSString*) myString
+{
+    //returns the length of the string times ten
+    return [myString length] * 10;
+}
+-(NSArray *) addThisStringToAnArray:(NSString*) firstString andThisString:(NSString*) secondString andAlsoThisString:(NSString*) thirdString
+{
+    return [NSArray arrayWithObjects: firstString, secondString, thirdString, nil];
+}
+    //touches screen
+[character flap];
